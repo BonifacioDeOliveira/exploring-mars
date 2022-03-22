0a03195475f26aff75711b6b0dd4b3fc8a014517
BonifacioDeOliveira <bonifaciodeoliveira@outlook.com>
2022-03-22 00:27:24 -0400

    Changes at while to make it cleaner

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ from mars import MarsMatrix
 
 def get_probe_inputs():
     probe_landing_coordinates = input()
-    if probe_landing_coordinates == '':
+    if probe_landing_coordinates:
+        probe_movement_coordinates = input()
+        return probe_landing_coordinates, probe_movement_coordinates
+    else:
         return None, None
-    probe_movement_coordinates = input()
-    return probe_landing_coordinates, probe_movement_coordinates
+
+
+def insert_probe(probe_landing_coordinates, probe_movement_coordinates, mars_matrix):
+    landing_line, landing_column, pointing_to = probe_landing_coordinates.split()
+    mars_matrix.insert_probe(int(landing_line), int(landing_column), pointing_to, probe_movement_coordinates)
 
 
 line_column = input()
-lines_number = int(line_column[0])
-columns_number = int(line_column[2])
-mars_matrix = MarsMatrix(lines_number, columns_number)
+lines_number, columns_number = line_column.split()
+mars_matrix = MarsMatrix(int(lines_number), int(columns_number))
 
 while True:
     probe_landing_coordinates, probe_movement_coordinates = get_probe_inputs()
     if probe_landing_coordinates:
-        line, column, pointing_to = probe_landing_coordinates.split()
-        mars_matrix.insert_probe(int(line), int(column), pointing_to, probe_movement_coordinates)
+        insert_probe(probe_landing_coordinates, probe_movement_coordinates, mars_matrix)
     else:
         mars_matrix.print_probes()
         break
